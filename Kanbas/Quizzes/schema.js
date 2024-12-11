3df3d4f5b2d3960f0c24266087f3720b8274ecd3
KodeJaiSurya <59638236+KodeJaiSurya@users.noreply.github.com>
2024-12-11 15:53:05 -0500

    Aneri Changes

--- Kanbas/Quizzes/schema.js
+++ Kanbas/Quizzes/schema.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const schema = new mongoose.Schema(
   {
-    _id: String,
+    // _id: String,
     title: String,
     description: String,
     quizType: String,
